d5ad801a5f7c3812c6eeaa4fc5e8a5e46c734506
MalikeChapman <ashchapman330@gmail.com>
2021-04-04 14:46:51 -0400

    Started the alpha release of the app

---
+++ GroupProject/GroceryListManager/app/src/main/java/com/example/grocerylistmanager/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.example.grocerylistmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText etUsername;
+    EditText etPassword;
+    Button btnSignUp;
+    Button btnSignIn;
+    Context mContext = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(mContext, SignUp.class);
+                startActivity(i);
+            }
+        });
+
+
+
+    }
+
+    public void toMainActivity(View view) {
+        Intent i = new Intent(mContext, MainActivity.class);
+        startActivity(i);
+    }
+}
