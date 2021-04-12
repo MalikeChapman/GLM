47ef6872543dfe411e10f9aa685ea45246d12af8
MalikeChapman <ashchapman330@gmail.com>
2021-04-12 00:21:10 -0400

    Added an itemadapter class for the itemsearch recyclerview.

--- GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/Item.java
+++ GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/Item.java
@@ -4,5 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Item {
-    List<Item> itemList = new ArrayList<>();
+    String itemName;
+    double itemValue;
+    String itemDescription;
+
+
+    public Item(String itemName, double itemValue, String itemDescription) {
+        this.itemName = itemName;
+        this.itemValue = itemValue;
+        this.itemDescription = itemDescription;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public double getItemValue() {
+        return itemValue;
+    }
+
+    public void setItemValue(double itemValue) {
+        this.itemValue = itemValue;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
 }
--- GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/ItemAdapter.java
+++ GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/ItemAdapter.java
@@ -1,4 +1,79 @@
 package edu.qc.seclass.glm;
 
-public class ItemAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.grocerylistmanager.R;
+
+import java.util.List;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
+    Context context;
+    List<Item> items;
+
+    public ItemAdapter(Context context, List<Item> items){
+        this.context = context;
+        this.items = items;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context).inflate(R.layout.itemview, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Item item = items.get(position);
+        holder.bind(item);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ConstraintLayout container;
+        EditText etQuantityNumber;
+        ImageView ivItemPicture;
+        ImageView ivPlusQuantity;
+        ImageView ivMinusQuantity;
+        ImageView ivAddItemToList;
+        TextView tvItemName;
+        TextView tvItemPrice;
+
+
+
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            container = itemView.findViewById(R.id.container);
+            etQuantityNumber = itemView.findViewById(R.id.etQuantityNumber);
+            ivItemPicture = itemView.findViewById(R.id.ivItemPicture);
+            ivPlusQuantity = itemView.findViewById(R.id.ivPlusQuantity);
+            ivMinusQuantity = itemView.findViewById(R.id.ivMinusQuantity);
+            ivAddItemToList = itemView.findViewById(R.id.ivAddItemToList);
+            tvItemName = itemView.findViewById(R.id.tvItemName);
+            tvItemPrice = itemView.findViewById(R.id.tvItemPrice);
+        }
+
+        public void bind(final Item item){
+            tvItemName.setText(item.getItemName());
+            tvItemPrice.setText(Double.toString(item.getItemValue()));
+
+
+        }
+    }
 }
